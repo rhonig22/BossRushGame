48139647859738d8540657ec4cc7b9169ca324ae
rhonig22 <ryan.honig22@outlook.com>
2024-01-17 16:59:06 -0800

    Initial Swappable Abilities
    - base ability that can be extended
    - logic to swap abilities
    - basic scratch and dodge abilities

---
+++ Assets/Scripts/Player/Abilities/BaseAbility.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseAbility : MonoBehaviour
+{
+    public AbilityType AbilityType;
+    public string Name = string.Empty;
+    protected Rigidbody2D _playerRB;
+    protected PlayerController _playerController;
+
+    private void Awake()
+    {
+        _playerRB = GetComponent<Rigidbody2D>();
+        _playerController = GetComponent<PlayerController>();
+    }
+
+    public virtual void ActivateAbility()
+    {
+
+    }
+}
+
+public enum AbilityType
+{
+    Scratch = 0,
+    Dodge = 1,
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,21 +5,47 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float _horizontalInput, _verticalInput;
+    private bool _ability1Pressed, _ability2Pressed = false;
     private readonly int _speed = 15;
     private readonly float _movementSmoothing = .1f;
     private Vector2 _currentVelocity = Vector2.zero;
+    private BaseAbility _ability1, _ability2;
+    private Dictionary<AbilityType, BaseAbility> _abilityMap = new Dictionary<AbilityType, BaseAbility>();
+    [SerializeField] private AbilityType _ability1Type = AbilityType.Dodge;
+    [SerializeField] private AbilityType _ability2Type = AbilityType.Scratch;
     [SerializeField] private Rigidbody2D _playerRB;
+
+    private void Awake()
+    {
+        BaseAbility[] abilities = GetComponentsInChildren<BaseAbility>();
+        foreach (BaseAbility ability in abilities)
+        {
+            _abilityMap.Add(ability.AbilityType, ability);
+        }
+
+        SetAbilities();
+    }
 
     // Update is called once per frame
     void Update()
     {
         _horizontalInput = Input.GetAxisRaw("Horizontal");
         _verticalInput = Input.GetAxisRaw("Vertical");
+        if (Input.GetButtonDown("Ability1"))
+            _ability1Pressed = true;
+
+        if (Input.GetButtonDown("Ability2"))
+            _ability2Pressed = true;
     }
 
     private void FixedUpdate()
     {
         Move(_horizontalInput, _verticalInput);
+        if (_ability1Pressed)
+            ActivateAbility1();
+
+        if (_ability2Pressed)
+            ActivateAbility2();
     }
 
     private void Move(float xSpeed, float ySpeed)
@@ -35,5 +61,25 @@ public class PlayerController : MonoBehaviour
     private void SetRotation(Vector3 direction)
     {
         _playerRB.transform.rotation = Quaternion.LookRotation(transform.forward, direction);
+    }
+
+    private void SetAbilities()
+    {
+        _ability1 = _abilityMap[_ability1Type];
+        _ability2 = _abilityMap[_ability2Type];
+    }
+
+    private void ActivateAbility1()
+    {
+        _ability1Pressed = false;
+        if (_ability1 != null)
+            _ability1.ActivateAbility();
+    }
+
+    private void ActivateAbility2()
+    {
+        _ability2Pressed = false;
+        if (_ability2 != null)
+            _ability2.ActivateAbility();
     }
 }
