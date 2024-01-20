d845d0f7d1a5566b1feba7b27080c88d12ba116c
rhonig22 <ryan.honig22@outlook.com>
2024-01-19 16:02:25 -0800

    Updated with changes
    - Kept all scratch logic in the ability file
    - Use overlapBoxAll rather than trigger area for assigning damage
    - Calculate lerpspeed of healthbar

--- Assets/Scripts/Player/Abilities/ScratchAbility.cs
+++ Assets/Scripts/Player/Abilities/ScratchAbility.cs
@@ -5,21 +5,60 @@ using UnityEngine;
 public class ScratchAbility : BaseAbility
 {
     private readonly float _scratchTime = .75f;
+    private readonly int _scratchDamage = 20;
+    private PlayerController _playerController;
     [SerializeField] private GameObject _scratchArea;
 
-    private void Awake()
+    private void Start()
     {
+        _playerController = GetComponent<PlayerController>();
     }
 
     public override void ActivateAbility()
     {
         base.ActivateAbility();
+        SetScratchPosition();
         _scratchArea.SetActive(true);
+        DetectEnemyCollision();
         StartCoroutine(EndScratch());
     }
     private IEnumerator EndScratch()
     {
         yield return new WaitForSeconds(_scratchTime);
         _scratchArea.SetActive(false);
+    }
+
+    private void SetScratchPosition()
+    {
+        var direction = _playerController.CurrentDirection;
+        if (Mathf.Abs(direction.x) > 0)
+        {
+            _scratchArea.transform.localScale = new Vector3(.5f, 1.5f, 1f);
+            if (direction.x < 0)
+                _scratchArea.transform.localPosition = new Vector3(-.75f, 0, 0);
+            else
+                _scratchArea.transform.localPosition = new Vector3(.75f, 0, 0);
+        }
+        else
+        {
+            _scratchArea.transform.localScale = new Vector3(1.5f, .5f, 1f);
+            if (direction.y < 0)
+                _scratchArea.transform.localPosition = new Vector3(0, -.75f, 0);
+            else
+                _scratchArea.transform.localPosition = new Vector3(0, .75f, 0);
+        }
+
+    }
+
+    private void DetectEnemyCollision()
+    {
+        Collider2D[] colliders = Physics2D.OverlapBoxAll(_scratchArea.transform.position, _scratchArea.transform.localScale, 0, LayerMask.NameToLayer("Enemy"));
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.CompareTag("Enemy"))
+            {
+                collider.GetComponent<BossHealth>().TakeDamage(_scratchDamage);
+            }
+        }
     }
 }
