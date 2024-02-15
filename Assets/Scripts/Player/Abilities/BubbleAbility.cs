028e7ec22736de172631eb94e6232fd74ce34f14
rhonig22 <ryan.honig22@outlook.com>
2024-02-15 13:50:12 -0800

    Charge Bubble
    - charge bubble on hold
    - bubble increases size and travels farther based on charge time
    - fixed a couple ux bugs

--- Assets/Scripts/Player/Abilities/BubbleAbility.cs
+++ Assets/Scripts/Player/Abilities/BubbleAbility.cs
@@ -1,19 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BubbleAbility : BaseAbility
 {
     [SerializeField] private GameObject _bubble;
     [SerializeField] private AudioClip _bubbleSpitSound;
-    private float _bubbleDelay = 0.25f;
-    private float _magnitude = 6f;
+    private readonly float _bubbleDelay = 0.25f, _minMagnitude = 1f, _maxMagnitude = 7f, _minSize = .5f, _maxSize = 1.5f, _maxCharge = 1f;
+    private float _magnitude = 1f, _timeCharged = 0;
+    private bool _isBubbleCharging = false;
+    private GameObject _tempBubble;
+    private BubbleController _tempBubbleController;
+
+    private void Update()
+    {
+        if (_isBubbleCharging)
+        {
+            _timeCharged += Time.deltaTime;
+            _magnitude = Mathf.Lerp(_minMagnitude, _maxMagnitude, _timeCharged / _maxCharge);
+            _tempBubble.transform.localScale = Vector3.Lerp(Vector3.one * _minSize, Vector3.one * _maxSize, _timeCharged / _maxCharge);
+        }
+    }
 
     public override void ActivateAbility()
     {
         base.ActivateAbility();
+        _isBubbleCharging = true;
+        _magnitude = _minMagnitude;
+        _timeCharged = 0;
         CreateBubble();
         _playerController.HaltMovement();
+    }
+
+    public override void EndAbility()
+    {
+        base.EndAbility();
+        SendBubble();
+        _isBubbleCharging = false;
     }
 
     private void CreateBubble()
@@ -22,9 +46,18 @@ public class BubbleAbility : BaseAbility
         var bubbleController = bubble.GetComponent<BubbleController>();
         bubbleController.BubbleSent.AddListener(() => { EndBubble(); });
         SetBubblePosition(bubble);
-        var endPosition = bubble.transform.position + _playerController.CurrentDirection * _magnitude;
+        _tempBubble = bubble;
+        _tempBubbleController = bubbleController;
+    }
+
+    private void SendBubble()
+    {
+        var endPosition = _tempBubble.transform.position + _playerController.CurrentDirection * _magnitude;
         SoundManager.Instance.PlaySound(_bubbleSpitSound, transform.position);
-        bubbleController.SendBubbleWithDelay(_bubbleDelay, endPosition);
+        _tempBubbleController.SendBubbleWithDelay(_bubbleDelay, endPosition);
+
+        _tempBubble = null;
+        _tempBubbleController = null;
     }
 
     private void EndBubble()
@@ -36,6 +69,7 @@ public class BubbleAbility : BaseAbility
     {
         var direction = _playerController.CurrentDirection;
         bubble.transform.position = transform.position;
+        bubble.transform.localScale = new Vector3(_minSize, _minSize, 1f);
         if (Mathf.Abs(direction.x) > 0)
         {
             if (direction.x < 0)
